ff708d1573c424988fa5b0a33354b5363205e4d0
Linad <merzlikindaniil97@gmail.com>
2024-04-13 15:41:57 +0300

    Chat done

---
+++ chat-server/src/main/java/org/example/chat/server/ClientManager.java
@@ -0,0 +1,85 @@
+package org.example.chat.server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientManager implements Runnable {
+    private final Socket socket;
+    private String name;
+    private BufferedWriter bufferedWriter;
+    private BufferedReader bufferedReader;
+    public static ArrayList<ClientManager> clients = new ArrayList<>();
+
+    public ClientManager(Socket socket) {
+        this.socket = socket;
+        try {
+
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.name = bufferedReader.readLine();
+            clients.add(this);
+            System.out.println(name + " conected");
+            sendClientMassage(name + " conected");
+        } catch (IOException e) {
+            closeEvery(socket, bufferedWriter, bufferedReader);
+        }
+    }
+
+    private void removeClient() {
+        clients.remove(this);
+        System.out.println(name + " disconnected");
+        sendClientMassage(name + " disconnected");
+    }
+
+    private void closeEvery(Socket socket, BufferedWriter bufferedWriter, BufferedReader bufferedReader) {
+        removeClient();
+        try {
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    private void sendClientMassage(String massage) {
+        for (ClientManager client : clients) {
+            try {
+
+                if (!client.name.equals(name) && massage != null) {
+                    client.bufferedWriter.write(massage);
+                    client.bufferedWriter.newLine();
+                    client.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                closeEvery(socket, bufferedWriter, bufferedReader);
+            }
+        }
+    }
+
+    /**
+     * Чтение сообщений от клиентов
+     */
+    @Override
+    public void run() {
+        String massageFromClient;
+        while (socket.isConnected()) {
+            try {
+                //Чтение данных
+                massageFromClient = bufferedReader.readLine();
+                sendClientMassage(massageFromClient);
+            } catch (IOException e) {
+                closeEvery(socket, bufferedWriter, bufferedReader);
+                break;
+            }
+
+        }
+    }
+}
